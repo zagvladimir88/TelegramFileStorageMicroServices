1bcb4c56473a3d7fe49bf67a3c1344cce04f5581
Vladimir Zagorny <zagvladimir88@gmail.com>
2023-03-01 22:02:28 +0300

    UpdateController, rabbit functionality

---
+++ dispatcher/src/main/java/com/zagvladimir/service/UpdateProducer.java
@@ -0,0 +1,7 @@
+package com.zagvladimir.service;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public interface UpdateProducer {
+    void produce(String rabbitQueue, Update update);
+}
